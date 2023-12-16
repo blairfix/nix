1585dfa372b7e2f61924438d639f525505e05e4e
blairfix <blairfix@gmail.com>
2023-12-16 09:55:34 -0700

    r packages

--- r_packages.nix
+++ r_packages.nix
@@ -6,14 +6,61 @@
 	R-with-my-packages = rWrapper.override{ 
 	    packages = with rPackages; [ 
 
-		ggplot2 
-		#dplyr 
-		#data_table
-		#Rcpp
-		#RcppArmadillo
-		#BH
-		#mailR
-		#sf
+		tidyverse
+		BH
+		data_table
+		doSNOW
+		cowplot
+		foreach
+		ggrepel
+		gridExtra
+		gtable
+		haven
+		here
+		lexicon
+		magrittr
+		stringi
+		mailR
+		microbenchmark
+		ngramr
+		oce
+		osfr
+		png
+		pracma
+		progress
+		readxlsb
+		rlecuyer
+		R_utils
+		Rcpp
+		RcppArmadillo
+		RcppGSL
+		RcppZiggurat
+		scales
+		snow
+		VGAM
+		xtable
+		wordcloud2
+		ggpubr
+		qdapRegex
+		fastmatch
+		webshot
+		RcppRoll
+		tm
+		ggcorrplot
+		xlsx
+		sf
+		igraph
+		tidygraph
+		tidyquant
+		ggraph
+		ggiraph
+		roll
+		poweRlaw
+		magick
+		truncnorm
+		fuzzyjoin
+		ggtext
+
 	    ];
 	};
     in [
