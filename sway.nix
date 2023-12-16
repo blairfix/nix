9567fe7b732cad13ffba9b3f280abe0f45fb111d
blairfix <blairfix@gmail.com>
2023-12-15 23:16:15 -0700

    removed i3

--- sway.nix
+++ sway.nix
@@ -34,31 +34,6 @@
     };
 
 
-  services.xserver = {
-    enable = true;
-
-    desktopManager = {
-      xterm.enable = false;
-    };
-   
-    displayManager = {
-        defaultSession = "none+i3";
-    };
-
-    windowManager.i3 = {
-      enable = true;
-      extraPackages = with pkgs; [
-        dmenu 
-        i3status 
-        i3lock 
-        i3blocks 
-     ];
-    };
-  };
-
-
-
-
     # CUPS to print documents.
     services.printing.enable = true;
 
@@ -107,14 +82,6 @@
 
     # system packages
     environment.systemPackages = with pkgs; [
-
-	# xorg, i3 and associated utilities
-	i3blocks 
-	i3status 
-	redshift
-	wmctrl
-	dmenu
-	picom
 
 	# wayland, sway and associated utilities
 	sway
