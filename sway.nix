8b8930655021bce579473e1a7dffde20c38f30dd
blairfix <blairfix@gmail.com>
2023-12-15 23:12:35 -0700

    i3

--- sway.nix
+++ sway.nix
@@ -33,14 +33,31 @@
 	wrapperFeatures.gtk = true;
     };
 
-    # i3 
-    services.xserver = {
-	enable = true;
+
+  services.xserver = {
+    enable = true;
+
+    desktopManager = {
+      xterm.enable = false;
+    };
+   
+    displayManager = {
+        defaultSession = "none+i3";
     };
 
     windowManager.i3 = {
       enable = true;
-    }
+      extraPackages = with pkgs; [
+        dmenu 
+        i3status 
+        i3lock 
+        i3blocks 
+     ];
+    };
+  };
+
+
+
 
     # CUPS to print documents.
     services.printing.enable = true;
