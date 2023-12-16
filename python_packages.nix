2bba09732c907b52512aaacb566f17f3eaee8958
blairfix <blairfix@gmail.com>
2023-12-16 09:32:48 -0700

    actually added python

---
+++ python_packages.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+{
+    environment.systemPackages = with pkgs  [ 
+
+	(
+	 python3.withPackages(
+	     ps: with ps; [ 
+
+		pandas 
+		datetime
+		tqdm
+		grip
+		imutils
+		linode-cli
+		python310Packages.selenium
+		python310Packages.radian
+
+	    ])
+	)
+    ];
+}
