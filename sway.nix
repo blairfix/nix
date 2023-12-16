6becc7290082e6d70a099c57812d4ad36eb64a5c
blairfix <blairfix@gmail.com>
2023-12-15 23:07:47 -0700

    i3

--- sway.nix
+++ sway.nix
@@ -33,6 +33,14 @@
 	wrapperFeatures.gtk = true;
     };
 
+    # i3 
+    services.xserver = {
+	enable = true;
+    };
+
+    windowManager.i3 = {
+      enable = true;
+    }
 
     # CUPS to print documents.
     services.printing.enable = true;
@@ -84,14 +92,10 @@
     environment.systemPackages = with pkgs; [
 
 	# xorg, i3 and associated utilities
-	xorg-server 
-	xorg-xinit 
-	i3-wm 
 	i3blocks 
 	i3status 
 	redshift
 	wmctrl
-	xorg-xkill
 	dmenu
 	picom
 
