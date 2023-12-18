2f6c9f780056ed84a990f596530a3e6a85c8f680
blairfix <blairfix@gmail.com>
2023-12-17 20:48:52 -0700

    more timers

--- system_server_timers.nix
+++ system_server_timers.nix
@@ -86,6 +86,119 @@
 	    ExecStart="/home/blair/cronjobs/active/rclone";
 	};
     };
-  
+
+
+    # forbes 
+    #----------------------------------------
+
+    systemd.timers."forbes" = {
+	wantedBy = [ "timers.target" ];
+	timerConfig = {
+	    OnCalendar= "*-*-* 21:38:00";
+	    AccuracySe c= "1sec";
+	    Persistent = "true";
+	    Unit = "forbes.service";
+	};
+    };
+
+    systemd.services."forbes" = {
+	serviceConfig = {
+	    Type = "simple";
+	    User = "blair";
+	    ExecStart = "/home/blair/Dropbox/cloud_work/empirical_research/scrape_forbes/run.sh";
+	};
+    };
+
+
+    # mastodon 
+    #----------------------------------------
+
+    systemd.timers."mastodon" = {
+	wantedBy = [ "timers.target" ];
+	timerConfig = {
+	    OnCalendar = "*-*-* 05:30:00";
+	    RandomizedDelaySec = "400";
+	    Persistent = "true";
+	    Unit = "mastodon.service";
+	};
+    };
+
+    systemd.services."mastodon" = {
+	serviceConfig = {
+	    Type = "simple";
+	    User = "blair";
+	    ExecStart = "/home/blair/Projects/scrape_mastodon/download_followers.py";
+	};
+    };
+
+
+
+    # twitter_scrape 
+    #----------------------------------------
+
+    systemd.timers."twitter_scrape" = {
+	wantedBy = [ "timers.target" ];
+	timerConfig = {
+	    OnCalendar = "*-*-* 05:00:00";
+	    RandomizedDelaySec = "400";
+	    Persistent = "true";
+	    Unit = "twitter_scrape.service";
+	};
+    };
+
+    systemd.services."twitter_scrape" = {
+	serviceConfig = {
+	    Type = "simple";
+	    User = "blair";
+	    ExecStart = "/home/blair/Projects/scrape_twitter/download_followers_6.py";
+	};
+    };
+
+
+    # twitter_email 
+    #----------------------------------------
+
+    systemd.timers."twitter_email" = {
+	wantedBy = [ "timers.target" ];
+	timerConfig = {
+	    OnCalendar = "*-*-01 07:30:00";
+	    RandomizedDelaySec = "400";
+	    Persistent = "true";
+	    Unit = "twitter_email.service";
+	};
+    };
+
+    systemd.services."twitter_email" = {
+	serviceConfig = {
+	    Type = "simple";
+	    User = "blair";
+	    ExecStart = "/home/blair/Projects/scrape_twitter/email/send_analysis.sh";
+	};
+    };
+
+
+    # wordpress_scrape 
+    #----------------------------------------
+
+    systemd.timers."wordpress_scrape" = {
+	wantedBy = [ "timers.target" ];
+	timerConfig = {
+	    OnCalendar = "*-*-02 05:30:00";
+	    RandomizedDelaySec = "400";
+	    Persistent = "true";
+	    Unit = "wordpress_scrape.service";
+	};
+    };
+
+    systemd.services."wordpress_scrape" = {
+	serviceConfig = {
+	    Type = "simple";
+	    User = "blair";
+	    ExecStart = "/home/blair/Projects/scrape_wordpress/RUNALL.sh";
+	};
+    };
+
+
+
 
 }
