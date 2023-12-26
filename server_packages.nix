196cdb66df679f149a9ec235064ebf392eeccca0
blairfix <blairfix@gmail.com>
2023-12-26 16:38:56 -0700

    removed photoprism

--- server_packages.nix
+++ server_packages.nix
@@ -1,28 +1,6 @@
 { pkgs, ... }:
 {
 
-    # photoprism
-    #-------------------------------------------
-
-    services.photoprism = {
-	enable = true;
-	port = 2342;
-	originalsPath = "/var/lib/private/photoprism/originals";
-	address = "0.0.0.0";
-	settings = {
-	    PHOTOPRISM_ADMIN_USER = "admin";
-	    PHOTOPRISM_ADMIN_PASSWORD = "...";
-	    PHOTOPRISM_DEFAULT_LOCALE = "en";
-	    PHOTOPRISM_DATABASE_DRIVER = "mysql";
-	    PHOTOPRISM_DATABASE_NAME = "photoprism";
-	    PHOTOPRISM_DATABASE_SERVER = "/run/mysqld/mysqld.sock";
-	    PHOTOPRISM_DATABASE_USER = "photoprism";
-	    PHOTOPRISM_SITE_URL = "http://sub.domain.tld:2342";
-	    PHOTOPRISM_SITE_TITLE = "My PhotoPrism";
-	};
-    };
-
-
     # jellyfin
     #-------------------------------------------
 
