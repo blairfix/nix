2a363cf86495c226ec16654330eb1e05b43dc53a
blairfix <blairfix@gmail.com>
2023-12-26 16:10:57 -0700

    added photoprism

--- server_packages.nix
+++ server_packages.nix
@@ -1,6 +1,26 @@
 { pkgs, ... }:
 {
 
+    # photoprism
+    services.photoprism = {
+	enable = true;
+	port = 2342;
+	originalsPath = "/var/lib/private/photoprism/originals";
+	address = "0.0.0.0";
+	settings = {
+	    PHOTOPRISM_ADMIN_USER = "admin";
+	    PHOTOPRISM_ADMIN_PASSWORD = "...";
+	    PHOTOPRISM_DEFAULT_LOCALE = "en";
+	    PHOTOPRISM_DATABASE_DRIVER = "mysql";
+	    PHOTOPRISM_DATABASE_NAME = "photoprism";
+	    PHOTOPRISM_DATABASE_SERVER = "/run/mysqld/mysqld.sock";
+	    PHOTOPRISM_DATABASE_USER = "photoprism";
+	    PHOTOPRISM_SITE_URL = "http://sub.domain.tld:2342";
+	    PHOTOPRISM_SITE_TITLE = "My PhotoPrism";
+	};
+    };
+
+    # jellyfin
     services.jellyfin = {
 	enable = true;
 	openFirewall = true;
@@ -13,7 +33,9 @@
 	pkgs.jellyfin-web
 	pkgs.jellyfin-ffmpeg
 
-
     ];
+
+
+
 
 }
